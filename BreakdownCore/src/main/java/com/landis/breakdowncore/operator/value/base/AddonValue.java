4c417bdc442898b64e50a428f428c6db5fe8573c
SSOragneCATY <jumaoz@qq.com>
2024-05-29 13:47:48 +0800

    Operator addon value write

--- BreakdownCore/src/main/java/com/landis/breakdowncore/operator/value/base/AddonValue.java
+++ BreakdownCore/src/main/java/com/landis/breakdowncore/operator/value/base/AddonValue.java
@@ -6,19 +6,19 @@ public class AddonValue {
     private final String addonName;
     private final double addonValue;
     private final BaseValue.ValueType TYPE;
-    public final boolean IS_MULTIPLY;
-    public final boolean IS_FINAL_CALCULATE;
+    public final boolean isMultiply;
+    public final boolean isFinalCalculate;
 
     public AddonValue(BaseValue.ValueType type, String addonName, double addonValue , boolean isMultiply, boolean isFinalCalculate){
         this.TYPE = type;
         this.addonName = addonName;
         this.addonValue = addonValue;
-        this.IS_MULTIPLY = isMultiply;
-        this.IS_FINAL_CALCULATE = isFinalCalculate;
+        this.isMultiply = isMultiply;
+        this.isFinalCalculate = isFinalCalculate;
     }
 
     public double addonToValue(double originValue){
-        if(!this.IS_MULTIPLY){
+        if(!this.isMultiply){
             return originValue + addonValue;
         }else{
             return originValue * addonValue;
@@ -30,13 +30,13 @@ public class AddonValue {
         tag.putString("AddonName", addonName);
         tag.putDouble("AddonValue", addonValue);
         tag.putString("ValueType", TYPE.name());
-        tag.putBoolean("IsMultiply", IS_MULTIPLY);
-        tag.putBoolean("IsFinalCalculate", IS_FINAL_CALCULATE);
+        tag.putBoolean("IsMultiply", isMultiply);
+        tag.putBoolean("IsFinalCalculate", isFinalCalculate);
         return tag;
     }
 
     public double removeToValue(double originValue){
-        if(!this.IS_MULTIPLY){
+        if(!this.isMultiply){
             return originValue - addonValue;
         }else{
             return originValue / addonValue;
