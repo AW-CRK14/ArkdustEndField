4424a3ddd25b1952d5a5f8b0e65b20189f5fc854
AW-CRK14 <1731936512@qq.com>
2024-06-02 20:01:51 +0800

    捣鼓mixin

--- ArkdustEndField/src/main/java/com/landis/arkdust/mixin/mui/ItemRenderMixin.java
+++ ArkdustEndField/src/main/java/com/landis/arkdust/mixin/mui/ItemRenderMixin.java
@@ -3,7 +3,6 @@ package com.landis.arkdust.mixin.mui;
 import com.mojang.blaze3d.vertex.PoseStack;
 import icyllis.arc3d.engine.DirectContext;
 import icyllis.arc3d.engine.DrawableInfo;
-import icyllis.modernui.mc.ContainerDrawHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.BakedModel;
 import org.joml.Matrix4f;
@@ -14,10 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(targets = "icyllis.modernui.mc.ContainerDrawHelper$DrawItem")
-public class ItemRender {
+public class ItemRenderMixin {
 
     @Inject(method = "draw", at = @At(value = "INVOKE",target = "Lcom/mojang/blaze3d/vertex/PoseStack;mulPoseMatrix(Lorg/joml/Matrix4f;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
     private void poseTrans(DirectContext dContext, DrawableInfo info, CallbackInfo ci, Minecraft minecraft, Matrix4f oldProjection, BakedModel model, boolean light2D, PoseStack localTransform){
         localTransform.scale(-1,1,1);
+    }
+
+    @Inject(method = "draw",at = @At(value = "HEAD"))
+    private void test(DirectContext dContext, DrawableInfo info, CallbackInfo ci){
+        System.out.println("mui mixin test");
     }
 }
