bc57042cb3b946b5a51c15855bad51e13fab3dd1
SSOragneCATY <jumaoz@qq.com>
2024-06-03 16:56:38 +0800

    OOC write

--- BreakdownCore/src/main/java/com/landis/breakdowncore/operator/value/ResumePanel.java
+++ BreakdownCore/src/main/java/com/landis/breakdowncore/operator/value/ResumePanel.java
@@ -1,13 +1,34 @@
 package com.landis.breakdowncore.operator.value;
 
 public class ResumePanel {
-    public static final ResumePanel VANGUARD = null;
-    public static final ResumePanel GUARD= null;
-    public static final ResumePanel DEFENDER= null;
-    public static final ResumePanel SNIPER= null;
-    public static final ResumePanel CASTER= null;
-    public static final ResumePanel MEDIC= null;
-    public static final ResumePanel SUPPORTER= null;
-    public static final ResumePanel SPECIALIST= null;
+    public final String operatorName;
+    public final OperatorOccupation operatorOccupation;
 
+    public ResumePanel(String operatorName, OperatorOccupation operatorOccupation){
+        this.operatorName = operatorName;
+        this.operatorOccupation = operatorOccupation;
+    }
+    
+
+    public static class OperatorOccupation{
+        public final Occupation occupation;
+        public final String occupationName;
+        public final StatsPanel defaultState;
+
+        public OperatorOccupation(Occupation occupation, String name, StatsPanel defaultState) {
+            this.occupation = occupation;
+            this.occupationName = name;
+            this.defaultState = defaultState;
+        }
+    }
+    public enum Occupation{
+        VANGUARD, // 先锋
+        GUARD, // 近卫
+        DEFENDER, // 重装
+        SNIPER, // 射手
+        CASTER, // 术士
+        MEDIC, // 医疗
+        SUPPORTER, // 辅助
+        SPECIALIST, // 特种
+    }
 }
