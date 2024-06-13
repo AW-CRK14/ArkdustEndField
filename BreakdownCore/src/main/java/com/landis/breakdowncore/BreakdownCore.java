9413456643b614d0fe48ccafc477cef3bce04527
SSOragneCATY <jumaoz@qq.com>
2024-06-13 22:20:33 +0800

    Loading Screen FIX

--- BreakdownCore/src/main/java/com/landis/breakdowncore/BreakdownCore.java
+++ BreakdownCore/src/main/java/com/landis/breakdowncore/BreakdownCore.java
@@ -1,55 +1,28 @@
 package com.landis.breakdowncore;
 
+
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ItemModelGenerator;
-import net.minecraft.client.resources.model.ModelManager;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.fml.loading.FMLLoader;
-import net.neoforged.fml.loading.moddiscovery.MinecraftLocator;
-import net.neoforged.fml.loading.targets.CommonLaunchHandler;
-import net.neoforged.fml.loading.targets.FMLClientLaunchHandler;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.common.NeoForgeMod;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
+
+import java.io.IOException;
 
 @Mod(BreakdownCore.MODID)
 public class BreakdownCore {
     public static final String MODID = "brea";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public BreakdownCore(IEventBus modEventBus) {
+    public BreakdownCore(IEventBus modEventBus) throws IOException {
 
         Registries.ITEM.register(modEventBus);
         Registries.TAB.register(modEventBus);
         Registries.MaterialReg.MATERIAL.register(modEventBus);
         Registries.MaterialReg.FEATURE.register(modEventBus);
         Registries.MaterialReg.TYPE.register(modEventBus);
-
     }
 
     private static ItemModelGenerator ITEM_MODELGEN;
--- BreakdownCore/src/main/java/com/landis/breakdowncore/mixin/NeoLoadingClientMixin.java
+++ BreakdownCore/src/main/java/com/landis/breakdowncore/mixin/NeoLoadingClientMixin.java
@@ -2,41 +2,19 @@ package com.landis.breakdowncore.mixin;
 
 
 
-import com.landis.breakdowncore.BreakdownCore;
-import com.landis.breakdowncore.util.LoadingRenderUtil;
-import com.mojang.blaze3d.platform.GlConst;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.*;
-import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.LoadingOverlay;
-import net.minecraft.client.gui.screens.Overlay;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ReloadInstance;
-import net.minecraft.util.FastColor;
-import net.minecraft.util.Mth;
-import net.neoforged.fml.earlydisplay.ColourScheme;
-import net.neoforged.fml.earlydisplay.DisplayWindow;
-import net.neoforged.fml.earlydisplay.RenderElement;
 import net.neoforged.fml.loading.progress.ProgressMeter;
 import net.neoforged.neoforge.client.loading.NeoForgeLoadingOverlay;
-import org.joml.Matrix4f;
-import org.lwjgl.opengl.GL30C;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.function.IntSupplier;
 
 
 @Mixin(NeoForgeLoadingOverlay.class)
